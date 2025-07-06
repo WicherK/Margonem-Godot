1dce3eac14a1e1f770533b37d398d115add3803f
WicherK <quietgamesstudio@gmail.com>
2025-07-06 23:21:37 +0200

    Better Tip
    Made better looking tip when hovering on enemy or NPC.

--- Scripts/tip.gd
+++ Scripts/tip.gd
@@ -5,9 +5,9 @@ var name_text : String
 var boss_status_text : String
 var level_text : String
 
-@onready var name_label = $VBoxContainer/Label
-@onready var boss_status_label = $VBoxContainer/Label2
-@onready var level_label = $VBoxContainer/Label3
+@onready var name_label = $VBoxContainer/Name_Label
+@onready var boss_status_label = $VBoxContainer/Boss_status_Label
+@onready var level_label = $VBoxContainer/Level_Label
 
 @onready var container = $VBoxContainer
 
@@ -16,11 +16,9 @@ func _process(delta: float) -> void:
 	
 	if boss_status_text == "":
 		boss_status_label.visible = false;
-		container.add_theme_constant_override("separation", -1)
 		
 	if level_text == "":
 		level_label.visible = false;
-		container.add_theme_constant_override("separation", 0)
 		
 	boss_status_label.text = boss_status_text
 	level_label.text = level_text
